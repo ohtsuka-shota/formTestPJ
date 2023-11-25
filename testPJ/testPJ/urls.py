d88c0a294e9aa37205ecd3000965eb5f189633fa
ohtsuka-shota <ohtsuka.honban@gmail.com>
2023-11-26 00:52:17 +0900

    sign upの機構構築

--- testPJ/testPJ/urls.py
+++ testPJ/testPJ/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from .views import homeView
+from .views import homeView, UserCreateView
 
 index_view = TemplateView.as_view(template_name="index.html")
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path("index/", login_required(index_view), name="index"),
     path("polls/", include("polls.urls")),
     path("auth/", include("django.contrib.auth.urls"), name="login"),
+    path("createuser/", UserCreateView.as_view(), name="userCreate"),
     path('', homeView.as_view(), name="home"),
 ]
--- testPJ/testPJ/views.py
+++ testPJ/testPJ/views.py
@@ -1,6 +1,14 @@
 # This is Project views.py
 
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 class homeView(TemplateView):
     template_name = "home.html"
+
+class UserCreateView(CreateView):
+    template_name = 'userCreate.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
