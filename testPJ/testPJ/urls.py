9284f9da9c4d8894d54377c8d09e63ee5fb8a799
ohtsuka-shota <ohtsuka.honban@gmail.com>
2023-11-25 14:02:40 +0900

    復旧02

--- testPJ/testPJ/urls.py
+++ testPJ/testPJ/urls.py
@@ -1,8 +1,16 @@
+# This is Project urls.py
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 from .views import homeView
 
+index_view = TemplateView.as_view(template_name="index.html")
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", homeView.as_view(), name="home"),
+    path('admin/', admin.site.urls),
+    path("index/", login_required(index_view), name="index"),
+    path("auth/", include("django.contrib.auth.urls"), name="login"),
+    path('', homeView.as_view(), name="home"),
 ]
