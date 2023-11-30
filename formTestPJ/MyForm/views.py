9c51c3addc3235ce7746ed0664d8d4a7773d7c3d
ohtsuka-shota <ohtsuka.honban@gmail.com>
2023-11-30 19:15:09 +0900

    change

--- formTestPJ/MyForm/views.py
+++ formTestPJ/MyForm/views.py
@@ -5,7 +5,7 @@ from .forms import MyForm
 class MyFormView(FormView):
     template_name = "MyForm.html"
     form_class = MyForm
-    success_url = reverse_lazy("myform/")
+    success_url = reverse_lazy("MyForm:MyFormPage")
 
     def form_valid(self, form):
         form.save()  
